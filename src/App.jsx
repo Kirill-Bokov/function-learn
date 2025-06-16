a02173aba5ea14443914c7267094e43082441be6
Kirill-Bokov <kirill37reg2@gmail.com>
2025-06-16 15:46:29 +0300

    fixes before pull

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Header from "@components/Header"
+import TopicRenderer from "@components/TopicRenderer"
+
+
+function App() {
+
+  return (
+    <>
+    <Header/>
+    <TopicRenderer/>
+    </>
+  )
+}
+
+export default App
